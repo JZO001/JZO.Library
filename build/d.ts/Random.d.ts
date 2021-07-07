96aad496237c0388e8929b5af7661a43d38ea1ed
Zoltan Juhasz <jzo@jzo.hu>
2021-07-07 12:33:37 +0200

    Missing type declarations

---
+++ build/d.ts/Random.d.ts
@@ -0,0 +1,9 @@
+﻿import { CastableStatic } from "./ObjectBase";
+
+export interface Random {
+}
+export interface RandomStatic<T extends Random> extends CastableStatic<T> {
+    Next(): number;
+    Next(minValue: number, maxValue: number): number;
+}
+declare var Random: RandomStatic<Random>;
